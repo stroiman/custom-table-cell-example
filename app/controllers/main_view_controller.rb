4db8dad2acffbd00afe1c9978f43378fe4b2cbb0
Peter Strøiman <peter.s@me.com>
2013-04-01 16:17:15 +0200

    Created UITableView

--- app/controllers/main_view_controller.rb
+++ app/controllers/main_view_controller.rb
@@ -1,5 +1,20 @@
 class MainViewController < UIViewController
-  def viewDidLoad
-    super
+  layout do
+    @table_view = UITableView.alloc.initWithFrame(self.view.bounds, style: UITableViewStylePlain)
+    @table_view.dataSource = self
+    @table_view.delegate = self
+    subview(@table_view)
+  end
+
+  def noOfSectionsInTableView(tableView)
+    1
+  end
+
+  def tableView(tableView, numberOfRowsInSection: section)
+    0
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    nil
   end
 end
